e9620ff2be72a38dbdd6ff18e967fbce5800a1cb
kms125690 <66993573+kms125690@users.noreply.github.com>
2023-11-05 23:59:46 +0800

    update output validation

--- bdd/Features/Steps/Test_steps.py
+++ bdd/Features/Steps/Test_steps.py
@@ -10,6 +10,18 @@ def step_impl(context):
 def step_impl(context):
     context.testpage.submitbtn_click()
 
+@given(': I Click on Edit button with the "(?P<order>.+)"-th row')
+def step_impl(context, order):
+    context.testpage.editbtn_click(order)
+
+@step(': I Click on Delete button with the "(?P<order>.+)"-th row')
+def step_impl(context, order):
+    context.testpage.delbtn_click(order)
+
+@step(': I Click on Back button')
+def step_impl(context):
+    context.testpage.backbtn_click()
+
 @step(': I Enter a "(?P<Name>.+)" in Name field')
 def step_impl(context, Name):
     context.yoyo = "hi"
@@ -24,3 +36,27 @@ def step_impl(context, Email):
 @step(': I Enter a "(?P<Message>.+)" in Message field')
 def step_impl(context, Message):
     context.testpage.input(Message,"message")
+
+@step(': I Clear Name field')
+def step_impl(context):
+    context.testpage.clear("name")
+
+@step(': I Clear Email field')
+def step_impl(context):
+    context.testpage.clear("email")
+
+@step(': I Clear Message field')
+def step_impl(context):
+    context.testpage.clear("message")
+
+@step(': Check the "(?P<order>.+)"-th row "(?P<Name>.+)", "(?P<Email>.+)", "(?P<Message>.+)"')
+def step_impl(context, order, Name, Email, Message):
+    context.testpage.check_edit_input(order, Name, Email, Message)
+
+@step(': Check the new input "(?P<Name>.+)", "(?P<Email>.+)", "(?P<Message>.+)"')
+def step_impl(context, Name, Email, Message):
+    context.testpage.check_new_input(Name, Email, Message)
+
+@step(': I wait sometime')
+def step_impl(context):
+    context.testpage.wait()
