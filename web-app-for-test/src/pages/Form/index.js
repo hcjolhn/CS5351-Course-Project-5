6f9bf6cc5e348dbfa95ead731d8cb6d46e86ffd0
Fergus Leung <fergusleung96@gmail.com>
2023-11-27 15:12:11 +0800

    Feature/update demo web style (#10)
    * code improve + css update
    
    * add feature of reading csv and writing to feature
    
    * change css to scss; add loader; css fix;

---
+++ web-app-for-test/src/pages/Form/index.js
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./index.scss";
+import "./loader.scss";
+import { mockData } from "../../data/mock-data";
+import NoDataIcon from './NoData';
+
+const Form = () =>{
+    const [forms, setFormList] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    
+    useEffect(() =>{
+        setLoading(true);
+        fetch("http://localhost:8000/forms",{method:"GET"})
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setFormList(data);
+            })
+            .catch((e)=>{
+                console.debug(e);
+            })
+            .finally(()=>{
+                setLoading(false);
+            })
+    }, [])
+    if (loading) {
+        return (
+            <div className="spinner">
+                <div className="ball" />
+                <p>Loading...</p>
+            </div>
+        );
+    }
+    async function handleDelete(id){
+        await fetch("http://localhost:8000/forms/"+id,
+            {method:"DELETE"}).then(res=> {
+                if(res.ok){
+                    setLoading(false);
+                    window.location.reload();
+                }else{
+                    console.log(res);
+                }
+            })
+            .catch(error=> console.log(error))
+    }
+
+
+    return (
+        <>
+        <div className="form-container">
+        <div className="welcome-container">
+            <div className="title-container">
+                <h1>Automated Tools - Home Page</h1>
+                <div style={{padding : "10px 0"}}>You can click create button here to submit the form.</div>
+                <div style={{padding : "10px 0"}}>All the submitted data will be shown below.</div>
+            </div>
+            <div className="button-container">
+                {/* <button className="back-button" onClick={()=>navigate(-1)}>Back</button> */}
+                <button className="back-button" onClick={()=>{
+                    if(!forms || (forms && forms.length === 0)){
+                        setFormList(mockData);
+                    }else{
+                        setFormList([]);
+                    }
+                }}>{(!forms || (forms && forms.length === 0))?"Mock":"Undo"}</button>
+                <button className="create-button" onClick={()=>navigate("/form_input")}>Create</button>
+            </div>
+        </div>
+            <table border={1} className="form-table">
+            <tr className="table-header-rows">
+                <th>Name</th>
+                <th>Email</th>
+                <th>Message</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            {forms && forms.map((item,i) =>(
+                <tr className={`${i % 2 === 0?"row form-table-odd":"row"}`}>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.message}</td>
+                    <td><Link to={"/form_input/?id="+item.id}><button>Edit</button></Link></td>
+                    <td><button onClick={()=>handleDelete(item.id)}>Delete</button></td>
+                </tr>
+            ))}
+            </table>
+            {
+                !forms || (forms && forms.length) === 0 && 
+                <div className="empty-table-content">
+                    <div className="no-content-container"><NoDataIcon /><div>No Data</div></div>
+                </div>
+            }
+        </div>
+        </>
+    );
+}
+
+export default Form;
